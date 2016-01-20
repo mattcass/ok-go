a7af8048e4ff328c91d2db5f1a8e5d56553685e9
Matt Cass <matthewcasserly@gmail.com>
2016-01-20 11:15:45 -0500

    now with more PHP

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
-// Include Gulp & Plugins
 
+// GULP & PLUGINS 
 var gulp          = require('gulp');
-var browserSync   = require('browser-sync').create();
+var browserSync   = require('browser-sync');
 var autoprefixer  = require('gulp-autoprefixer');
 var concat        = require('gulp-concat');
-var jshint        = require ('gulp-jshint');
+var jshint        = require('gulp-jshint');
 var sass          = require('gulp-sass');
 var uglify        = require('gulp-uglify');
 var stylish       = require('jshint-stylish');
 var imagemin      = require('gulp-imagemin');
 var plumber       = require('gulp-plumber');
 var notify        = require('gulp-notify');
+var connectPHP    = require('gulp-connect-php');
+
+// FILE STRUCTURE
+var jsFiles       = 'app/assets/javascripts/*.js';
+var sassFiles     = 'app/assets/stylesheets/sass/**/*.scss';                        
+var imageFiles    = 'app/assets/images/*.{png, jpg, gif}';
+var phpFiles      = 'app/**/*.php';
 
 var onError = function(err) {
     notify.onError({
@@ -22,24 +29,30 @@ var onError = function(err) {
     this.emit('end');
 }
 
-// declare file structure
+gulp.task('connectPHP', function(){
+  connectPHP.server({
+    hostname: '0.0.0.0',
+    port: 8000,
+    base: './app/'
+  });
+});
 
-var jsFiles       = 'assets/javascripts/*.js';
-var sassFiles     = 'assets/stylesheets/sass/**/*.scss';                        
-var imageFiles    = 'assets/images/*.{png, jpg, gif}';
-
-gulp.task('serve', function() {
-    browserSync.init({
-      server: {
-        baseDir: './'
-      }
-    });
+gulp.task('serve', ['connectPHP'], function(){
+  notify: true,
+  browserSync({
+    proxy: 'localhost:8000'
+  });
 });
 
 gulp.task('imagemin', function() {
   return gulp.src(imageFiles)
     .pipe(imagemin())
-    .pipe(gulp.dest('assets/images'));
+    .pipe(gulp.dest('./app/assets/images'));
+});
+
+gulp.task('php', function() {
+  return gulp.src(phpFiles)
+    .pipe(browserSync.stream());
 });
 
 gulp.task('styles', function() {
@@ -47,13 +60,8 @@ gulp.task('styles', function() {
     .pipe(plumber({errorHandler: onError}))
     .pipe(sass())
     .pipe(autoprefixer())
-    .pipe(gulp.dest('./assets/stylesheets/css'))
-    .pipe(browserSync.stream());
-});
-
-gulp.task('html', function() {
-  return gulp.src('./*.html')
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest('./app/assets/stylesheets/css'))
+     .pipe(browserSync.stream());
 });
 
 gulp.task('js', function() {
@@ -63,16 +71,15 @@ gulp.task('js', function() {
     .pipe(jshint.reporter(stylish))
     .pipe(uglify())
     .pipe(concat('app.min.js'))
-    .pipe(gulp.dest('./assets/javascripts/dist'))
+    .pipe(gulp.dest('./app/assets/javascripts/dist'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./*.html', ['html'] );
+  gulp.watch(phpFiles, ['php'] );
   gulp.watch(sassFiles, ['styles']);
   gulp.watch(jsFiles, ['js']);
   gulp.watch(imageFiles, ['imagemin']);
 });
 
-
-gulp.task('default', ['watch', 'styles', 'html', 'js', 'imagemin','serve']);
+gulp.task('default', ['watch', 'styles', 'php', 'js', 'imagemin', 'serve']);
